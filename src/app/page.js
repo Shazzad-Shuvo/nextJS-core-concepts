a46311fd3b7a942b4fe3a9736fb6dfdd4b9e4de9
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2024-11-20 23:10:46 +0600

    slight change to text in home page to check vercel auto build

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h6>Hello Next JS in Build</h6>
+      <h6>Hello Next JS in Builder</h6>
       <Heading></Heading>
       <button>Time to Learn</button>
     </div>
